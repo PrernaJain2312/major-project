4ccea76409b75a3c1975e74b7e7a4ea01828cb2a
Jain <jain@Jains-MacBook-Pro.local>
2020-04-18 17:06:23 +0530

    first push

---
+++ app.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from flask import Flask, request, render_template, jsonify
+import pickle
+from fastai import *
+from fastai.text import *
+from fastai.core import *
+
+
+# load model
+path = "/Users/jain/Desktop/model/major_env/"
+model= load_learner(path,"model.pkl")
+
+
+
+
+# app
+app = Flask(__name__)
+
+# routes
+@app.route('/', methods=['GET'])
+
+def predict():
+    # get data
+    data = request.args.get('tweet')
+    
+
+    # predictions
+    result = model.predict(data)
+
+    # send back to browser
+    output = {'result': int(result[0])}
+
+    # return data
+    return jsonify(output)
+
+
+if __name__ == '__main__':
+    app.run(port = 2370, debug=True)
